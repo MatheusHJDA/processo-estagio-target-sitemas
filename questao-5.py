6895c4475af800240201b0fd3984cf4139563938
Matheus Mendes <88910463+MatheusHJDA@users.noreply.github.com>
2024-11-08 19:31:30 -0300

    Update questao-5.py

--- questao-5.py
+++ questao-5.py
@@ -1,3 +1,8 @@
+#5) Escreva um programa que inverta os caracteres de um string.
+#IMPORTANTE:
+#a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+#b) Evite usar funções prontas, como, por exemplo, reverse;
+
 texto = input("Digite uma string para inverter: ")
 
 texto_invertido = ""
@@ -5,9 +10,3 @@ for i in range(len(texto) - 1, -1, -1):
     texto_invertido += texto[i]
 
 print("String invertida:", texto_invertido)
-
-
-#5) Escreva um programa que inverta os caracteres de um string.
-#IMPORTANTE:
-#a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
-#b) Evite usar funções prontas, como, por exemplo, reverse;
